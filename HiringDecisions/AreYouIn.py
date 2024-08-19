c75a366cd2d398b1e4a8aa59659cecd6f003b83e
Liza20002 <86048765+Liza20002@users.noreply.github.com>
2024-08-19 10:49:24 +0300

    Update AreYouIn.py

--- HiringDecisions/AreYouIn.py
+++ HiringDecisions/AreYouIn.py
@@ -24,11 +24,11 @@ alpha = 0.05
 #################################
 # Question 1
 #################################
-#We Want to find out the connetction between The skills level of the candidate, and his/her acceptence rate.
-#Our H0- Base assumption is that that Avg skill score of candidates that were accepted to work is 70
+#We Want to find out the connetction between the skills level of the candidate, and their acceptence rate.
+#Our H0 - Base assumption is that Avg skill score of candidates that were accepted to work is 70
 mu_0 = 70
 
-# Here we will take only those who were accepted
+# Here we will only consider those accepted
 accepted_candidates = HiringData[HiringData['HiringDecision'] == 1]
 
 
@@ -46,21 +46,21 @@ std_dev_skill_score = np.sqrt(unbiased_variance)
 # estimate  the mean SkillScore of accepted candidates
 mean_skill_score_accepted = accepted_candidates['SkillScore'].mean()
 
-#Here we will show that we know how to calc the t statistic on our own, and later we will do that using an imported function
+#Here we will show that we know how to calculkate the t statistic on our own, and later we will do that using an imported function
 t_statistic_skill = (mean_skill_score_accepted - mu_0) / (std_dev_skill_score/np.sqrt(num_of_accepted_cands))
 
 
 # Calculate the critical value for a two-tailed test
 critical_value = t.ppf(1 - alpha/2, df=num_of_accepted_cands - 1) 
 
-#Calculating the p-value for two tailed
-# We can see that they use CDF to find the values that are equal or greater(or smaller) then the t- stat we found
+#Calculating the p-value for the two tailed test
+#We can see that they use CDF to find the values that are equal or greater(or smaller) than the t-stat we found
 p_value = 2 * (1 - t.cdf(abs(t_statistic_skill), num_of_accepted_cands - 1)) 
 
 #The T-statistic is lower the the critical value t_statistic<critical_value so we reject the HO
 #################3 finished two tailed test
 
-# we think that you are going to need to have 60 in personality to get accepted. our new mu_0 is <=60,
+#we assume that you need to have 60 in personality to get accepted. our new mu_0 is <=60,
 mu_0 = 60
 #our H1 is that mu is > 60 
 
@@ -82,7 +82,7 @@ mu_0 = 15
 t_statistic, p_value = ttest_1samp(accepted_candidates['DistanceFromCompany'], mu_0)
 #Calc the critical value
 critical_value = -t.ppf(1 - alpha, num_of_accepted_cands - 1) 
-#We found that our t_Statistic is bigger than the critical value , so we except H0
+#We found that our t_Statistic is bigger than the critical value, so we accept H0
 
 
 #################################
@@ -91,31 +91,31 @@ critical_value = -t.ppf(1 - alpha, num_of_accepted_cands - 1)
 
 # hypothesis testing for variance 
 # Checking if skill score has to do with experiance in years
-# We'll begin by taking the group of those who have over 13 years of experiance years
+# We'll begin by taking the group of those with over 13 years of experience
 very_experianced = HiringData[HiringData['ExperienceYears']>13]
 amount_of_very_experianced = len(very_experianced)
-# we have 209 candidates in this group
-# We think that the skill score for this group is very high,and that the variance is not going to be big.
+#We have 209 candidates in this group
+#We think that the skill score for this group is very high,and that the variance is not going to be big.
 #First we will find the variance in SkillScore for the whole sample
 variance_whole_sample = np.var(HiringData['SkillScore'], ddof=1)
 #we know that the variance in SkillScore for the whole sample is 861.63
 #Our assumption is that because we have a group of people that are very expirienced,they are going to get very high skill score with a smaller variance than the whole test
-# our H0 for the variance of this group is <= 750, and we will check that with right sided test
+#Our H0 for the variance of this group is <= 750, and we will check that with right-sided test
 var_h0 = 750
-# we will reject H0 if the chi value of the estimation will be greater then the chi value of our critical value
+#We will reject H0 if the chi value of the estimation will be greater than the chi value of our critical value
 critical_value = chi2.ppf(1 - alpha, amount_of_very_experianced-1)
-# critical value is 242.64
+#Critical value is 242.64
 #We will find an estimator for the mean of their skill set
 mean_skill_experianced = very_experianced['SkillScore'].mean()
-#we found that the mean skill score of this group is 51.976(surprisingly low)
-# Now let's find the variance of the sample
+#We found that the mean skill score of this group is 51.976(surprisingly low)
+#Now let's find the variance of the sample
 unbiased_variance_for_very_experianced = np.var(very_experianced['SkillScore'], ddof=1)
 #our unbiased_variance_for_very_experianced is 756.56
-#Now we will find the value of our chi stat for variance, assuming the our H0 is true
+#Now we will find the value of our chi stat for variance, assuming the our H0 is correct
 chi_stat_var = (amount_of_very_experianced-1) * unbiased_variance_for_very_experianced / var_h0 
-# the chi value of our sample is 240.57,  lower then the critical value of 242.64 ,so we accept our H0 with significance level "Ramat Muvhakut" of 0.05
+#The chi value of our sample is 240.57, lower then the critical value of 242.64, so we accept our H0 with significance level ("Ramat Muvhakut") of 0.05
 print(chi_stat_var)
-#let's find the P-value
+#Let's find the P-value
 p_value = 1- chi2.cdf(chi_stat_var,amount_of_very_experianced-1)
 
 #the p_value is 1.0
@@ -127,13 +127,13 @@ p_value = 1- chi2.cdf(chi_stat_var,amount_of_very_experianced-1)
 # Question 3
 #################################
 
-# Calculate the proportion of accepted candidates in all of the sample 
+# Calculate the proportion of accepted candidates in the entire sample 
 proportion_accepted = HiringData['HiringDecision'].mean()
 
-#we found that 0.31 were accepted.
+#We found that 0.31 were accepted.
 #We are going to check if there is a difference between people in different ages
 #Using discritization we will split to age groups of 5(20-25,25-30...)
-#our H0 for acceptence rate of the group 25-30 is higher then the rest, because they are in their begining of their career and after their university studies
+#our H0 for acceptence rate of the group 25-30 is higher than the rest, because they are at the begining of their career and after completing a degree
 p0 = 0.31
 #Our H1 is that the acceptence rate will be lower
 #Define the age bins (starting from 20 and ending at 50)
@@ -151,27 +151,27 @@ z_critical_bottom =-norm.ppf(1-alpha)
 #Now we will find the p-value for our test 
 p_value_right_tailed = (norm.cdf(z_proportion))
 
-#we found that our p-value is 0.957, which is bigger than our significance level 0.05, so we fail to recect H0
+#we found that our p-value is 0.957, which is bigger than our significance level 0.05, so we fail to reject H0
 
 #################################
 # Question 4
 #################################
 
 #In this question we will determine if there is a connection between the recruitment strategy(categorial betweewn 1-3) to the personallity score the candidate recieved.
-#first we will check the mean personallity grades in all of the sample
+#First we will check the mean personallity grades in the entire sample
 mean_personallity_score = HiringData['PersonalityScore'].mean()
-#we found that the mean personallity score in the sample is 49.387
-#our H0 is that there is that there is a connection and that when the interview is aggresive, the personallity score is lower.
-# so we assume that the mu0 in aggresive interviews is going to be 45
+#We found that the mean personallity score in the sample is 49.387
+#Our H0 is that there is a connection and that when the interview is 'aggresive', the personallity score is lower.
+#So we assume that the mu0 in 'aggresive' interviews is going to be 45
 mu0 = 45
-#we will devide them by the strategy and check the personallity score they got
+#We will devide them by the strategy and check the personallity score they got
 agrresive_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==1]
 moderate_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==2]
 conservative_recruitment_strategy = HiringData[HiringData['RecruitmentStrategy']==3]
 
 #let's find the mean of personallity scores within the agressive recruitment group
 mean_personallity_agrresive_recruitment_strategy = agrresive_recruitment_strategy['PersonalityScore'].mean() 
-#we found mean of 49.676 (Higher than General score of the sample!!!!! surprising!!!!!)
+#we found mean of 49.676 (Higher than General score of the sample!!! Surprising!)
 #let's find an estimate to the variance of this group
 unbiased_variance_personallity_when_agressive = np.var(agrresive_recruitment_strategy['PersonalityScore'], ddof=1)
 std_dev_personallity_when_agressive = np.sqrt(unbiased_variance_personallity_when_agressive)
